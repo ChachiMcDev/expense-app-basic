1032d8958920da01f009ff9a83b1e31de4f75a49
Chuck Nelson <chachimcdev@gmail.com>
2021-05-11 08:05:07 -0700

    removing dummy data

--- src/app.js
+++ src/app.js
@@ -8,23 +8,8 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss'; 
 import 'react-dates/lib/css/_datepicker.css';
 
-import {addExpense} from './actions/expenses';
-import { setTextFilter, setSortByAmountFilter } from './actions/filters';
-
 
 const store = configureStore();
-
-//store.dispatch(addExpense({ description: 'Water bill', amount: 50000 }));
-//store.dispatch(addExpense({ description: 'Gas bill', amount: 7000}));
-//store.dispatch(addExpense({ description: 'mortgage', amount: 250000}));
-//store.dispatch(addExpense({ description: 'life', amount: 2300 }));
-//store.dispatch(setSortByAmountFilter());
-//store.dispatch(setTextFilter('water'));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log('from app load',  visibleExpenses);
-
 
 
 const jsx = (
